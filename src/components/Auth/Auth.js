5f894d63e99892197d6f164b54c80582d9e25369
Abdify <abdullah.fahimm08@gmail.com>
2021-06-10 18:48:56 +0600

    sign up, sign in form created

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,89 @@
-import React from 'react';
+import { Avatar, Button, Container, Grid, Paper, Typography } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+import React, { useState } from 'react';
 import Input from './Input';
+import useStyle from './styles';
 
 const Auth = () => {
 
-    const handleChange = () => {}
-    const handleShowPassword = () => {}
+    const classes = useStyle();
+    const [isSignup, setIsSignup] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
+    const handleChange = () => {}
+    const handleShowPassword = () => setShowPassword(!showPassword);
+    const switchMode = () => setIsSignup(!isSignup);
+    const handleSubmit = () => {}
     return (
-        <div>
-            <Input name="email" type="email" label="Email" handleChange={handleChange} handleShowPassword={handleShowPassword} />
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    {isSignup ? "Sign up" : "Sign in"}
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {isSignup && (
+                            <>
+                                <Input
+                                    name="firstName"
+                                    label="First Name"
+                                    handleChange={handleChange}
+                                    autoFocus
+                                    half
+                                />
+                                <Input
+                                    name="lastName"
+                                    label="Last Name"
+                                    handleChange={handleChange}
+                                    half
+                                />
+                            </>
+                        )}
+                        <Input
+                            name="email"
+                            label="Email Address"
+                            handleChange={handleChange}
+                            type="email"
+                        />
+                        <Input
+                            name="password"
+                            label="Password"
+                            handleChange={handleChange}
+                            type={showPassword ? "text" : "password"}
+                            handleShowPassword={handleShowPassword}
+                        />
+                        {isSignup && (
+                            <Input
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                handleChange={handleChange}
+                                type="password"
+                            />
+                        )}
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        {isSignup ? "Sign Up" : "Sign In"}
+                    </Button>
+                    
+                    <Grid container justify="center">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? "Already have an account? Sign in" : "No account yet? Sign up"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     );
 };
 
