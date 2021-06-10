2e1de2391b22a3d14c15bf9d48596e04e6100cb1
Abdify <abdullah.fahimm08@gmail.com>
2021-06-10 14:44:51 +0600

    layout refactored, router added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
-import useStyles from './styles';
-
+import { Container } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch])
     return (
-        <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h4" align="center">
-                    The Pursue Machine
-                </Typography>
-            </AppBar>
-
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch">
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <Router>
+            <Container maxwidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/auth">
+                        <Auth />
+                    </Route>
+                </Switch>
+            </Container>
+        </Router>
     );
 };
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+    const user = null;
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography
+                    component={Link}
+                    to="/"
+                    className={classes.heading}
+                    variant="h4"
+                    align="center"
+                >
+                    The Pursue Machine
+                </Typography>
+                
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user?.result ? (
+                    <div className={classes.profile}>
+                        <Avatar
+                            className={classes.purple}
+                            alt={user?.result.name}
+                            src={user?.result.imageUrl}
+                        >
+                            {user?.result.name.charAt(0)}
+                        </Avatar>
+                        <Typography className={classes.userName} variant="h6">
+                            {user?.result.name}
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            className={classes.logout}
+                            color="secondary"
+                            onClick={() => {}}
+                        >
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">
+                        Sign In
+                    </Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
