3fb4ed08a70888592ed020761243084dab432d7b
Abdify <abdullah.fahimm08@gmail.com>
2021-06-11 21:38:49 +0600

    jwt authentication done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,7 +7,6 @@ import useStyles from "./styles";
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
-        creator: "",
         title: "",
         message: "",
         tags: "",
@@ -21,10 +20,12 @@ const Form = ({ currentId, setCurrentId }) => {
 
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    const user = localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile'));
+
     const clear = () => {
         setCurrentId(null);
         setPostData({
-            creator: "",
             title: "",
             message: "",
             tags: "",
@@ -34,10 +35,20 @@ const Form = ({ currentId, setCurrentId }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(currentId) dispatch(updatePost(currentId, postData));
-        else dispatch(createPost(postData));
+        if(currentId) dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+        else dispatch(createPost({...postData, name: user?.result?.name}));
         clear();
     };
+
+    if(!user?.result?.name){
+        return(
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Please sign in to create project and like others.
+                </Typography>
+            </Paper>
+        )
+    }
 
     return (
         <Paper className={classes.paper}>
@@ -48,14 +59,6 @@ const Form = ({ currentId, setCurrentId }) => {
                 onSubmit={handleSubmit}
             >
                 <Typography variant="h6">{currentId ? "Edit" : "Create"} Project</Typography>
-                <TextField
-                    name="creator"
-                    variant="outlined"
-                    label="Creator"
-                    fullWidth
-                    value={postData.creator}
-                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-                />
                 <TextField
                     name="title"
                     variant="outlined"
